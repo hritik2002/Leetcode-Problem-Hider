a5cab332a70bca28c7251f2bb3db85cbe86c0a10
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2023-02-07 21:05:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// only works on https://leetcode.com/tag/{tag_name}/ page
-
 function HideSolvedProblems() {
   let tableRow = document.querySelectorAll(".reactable-data tr");
 
   for (let key in tableRow) {
       if (Number(key) >= 0) {
-          if (tableRow[key].children[0].getAttribute("value")) {
-              tableRow[key].children[0].style.display = "none";
+          if (tableRow[key].children[0].getAttribute("value") === "ac") {
+              tableRow[key].style.display = "none";
           }
       }
   }
